7ee53e7ea9f1f0edebee3feabc6b6dd5117731b8
Beata Markunas <beata.markunas@gmail.com>
2018-04-30 09:50:44 +0200

    Create UserList component

--- script.js
+++ script.js
@@ -37,7 +37,19 @@ class App extends React.Component {
   }
 }
 
+class UsersList extends React.Component {
+  get users() {
+    return this.props.users.map(user => <User key={user.id} user={user}/>);
+  }
 
+  render() {
+    return (
+      <div>
+        {this.users}
+      </div>
+    );
+  }
+}
 
 
 ReactDOM.render(
